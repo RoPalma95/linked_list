b709336c1b0ff3a86c82910c88c7e7a73309ff38
RoPalma95 <r.palma995@gmail.com>
2020-11-25 15:33:07 -0600

    Refactoring

--- linked_list.rb
+++ linked_list.rb
@@ -1,4 +1,4 @@
-require 'pry'
+# frozen_string_literal: true
 
 class LinkedList
   private
@@ -25,61 +25,58 @@ class LinkedList
 
   public
 
-  def append(value) # adds new node containing 'value' to the end of the list
+  def append(value)
     node = create_node(value)
     node.index = size
     if head.nil?
       self.head = node
       self.tail = head
     else
-      self.tail.next_node = node
+      tail.next_node = node
       self.tail = node
     end
   end
 
-  def prepend(value) # adds new node containing 'value' to the start of the list
+  def prepend(value)
     node = create_node(value)
     node.index = 1
     node.next_node = head
+    self.head = node
     if head.nil?
       self.head = node
       self.tail = head
-    else
-      self.head = node
     end
-    adjust_indices("p")
+    adjust_indices('i', head.next_node)
   end
 
-  def insert_at(value, index) # inserts the node with the provided 'value' at the given 'index'
+  def insert_at(value, index)
     if index > (size + 1)
       puts "Index has to be #{size + 1} or lower."
     elsif index == (size + 1)
-      self.append(value)
+      append(value)
     elsif index == 1
-      self.prepend(value)
+      prepend(value)
     else
-      previous_node = self.at(index - 1)
+      previous_node = at(index - 1)
       new_node = create_node(value, index)
-      next_node = self.at(index)
+      next_node = at(index)
       previous_node.next_node = new_node
       new_node.next_node = next_node
-      adjust_indices("i", next_node)
+      adjust_indices('i', next_node)
     end
   end
 
-  def at(index) # return the node at the given index
+  def at(index)
     if index > size
       "Index #{index} does not exist."
     else
       current_node = head
-      until current_node.index == index
-        current_node = current_node.next_node
-      end
+      current_node = current_node.next_node until current_node.index == index
       current_node
     end
   end
 
-  def pop() # removes the last element from the list
+  def pop
     current_node = head
     (size - 2).times do
       current_node = current_node.next_node
@@ -89,48 +86,63 @@ class LinkedList
     self.size -= 1
   end
 
-  def contains?(value) # boolean, true if the passed 'value' is in the list
-    current_node = head
-    until current_node.nil? || current_node.value == value
-      current_node = current_node.next_node
+  def remove_at(index)
+    if index > size
+      puts "Index has to be #{size} or lower."
+    elsif index == size
+      pop
+    elsif index == 1
+      self.head = at(2)
+      head.index = 1
+      adjust_indices('r', head.next_node)
+      self.size -= 1
+    else
+      previous_node = at(index - 1)
+      current_node = at(index)
+      next_node = current_node.next_node
+      previous_node.next_node = next_node
+      adjust_indices('r', next_node)
+      self.size -= 1
     end
+  end
+
+  def contains?(value)
+    current_node = head
+    current_node = current_node.next_node until current_node.nil? || current_node.value == value
     current_node.nil? ? false : true
   end
 
-  def find(value) # return index of the node containing 'value'
-    if self.contains?(value)
+  def find(value)
+    if contains?(value)
       current_node = head
-      until current_node.value == value
-        current_node = current_node.next_node
-      end
+      current_node = current_node.next_node until current_node.value == value
       current_node.index
     else
       "Value #{value} does not exist."
     end
   end
 
-  def adjust_indices(insert_method, next_node = nil) # fixes indices after prepending or inserting an element
-    if insert_method == 'p'
-      current_node = head.next_node
-      until current_node.nil?
-        current_node.index += 1
-        current_node = current_node.next_node
+  def adjust_indices(insert_remove, node = nil)
+    if insert_remove == 'i'
+      until node.nil?
+        node.index += 1
+        node = node.next_node
       end
     else
-      until next_node.nil?
-        next_node.index += 1
-        next_node = next_node.next_node
+      until node.nil?
+        node.index -= 1
+        node = node.next_node
       end
     end
   end
 
-  def to_s # represent LinkedList objects as strings: "( value ) -> ( value ) -> ( value ) -> nil"
-    if size == 0
-      puts 'The list is empty.'
+  def to_s
+    if size.zero?
+      'The list is empty.'
     else
       current_node = head
-      (size - 1).times do 
-        print"( #{current_node.value} ) -> "
+      (size - 1).times do
+        print "( #{current_node.value} ) -> "
         current_node = current_node.next_node
       end
       "( #{tail.value} ) -> nil"
@@ -149,14 +161,33 @@ class Node
 end
 
 my_list = LinkedList.new
-my_list.append(1)
-my_list.append(2)
-my_list.prepend(3)
-my_list.append(4)
-my_list.append(5)
-my_list.prepend(6)
-my_list.insert_at(7, 2)
-my_list.pop
+
+10.times do |i|
+  my_list.append(i + 1)
+end
 
 puts my_list
-puts my_list.size
+
+my_list.append(11)
+puts my_list
+
+my_list.prepend(0)
+puts my_list
+
+my_list.insert_at(5.75, 7)
+puts my_list
+puts my_list.find(10)
+
+my_list.pop
+puts my_list
+
+my_list.remove_at(7)
+puts my_list
+
+my_list.remove_at(1)
+puts my_list
+
+puts my_list.contains?(-1)
+puts my_list.contains?(7)
+
+puts my_list.find(10)
